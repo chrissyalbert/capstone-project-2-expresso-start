ae1c63d90fd72a9f17a50752296dbcb9616efd7f
chrissyalbert <chrissy_albert@yahoo.com>
2019-09-12 16:28:07 -0700

    Add dependencies to server.js, add database to migration.js

---
+++ migration.js
@@ -0,0 +1,8 @@
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./database.sqlite');
+
+db.serialize(function() {
+  db.run(
+    
+  );
+});
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const errorhandler = require('errorhandler');
+const morgan = require('morgan');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(errorhandler());
+
+app.listen(PORT, function () {
+  console.log('CORS-enabled web server listening on port 4000');
+});
+
+module.exports = app;
