8589e9562d2d4e300f6b474c4dfe947cfa352876
chrissyalbert <chrissy_albert@yahoo.com>
2019-09-17 15:02:01 -0700

    Add timesheetsRouter put route

--- api/timesheets.js
+++ api/timesheets.js
@@ -4,6 +4,23 @@ const timesheetsRouter = express.Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+timesheetsRouter.param('timesheetId', (req, res, next, timesheetId) => {
+  const sql = 'SELECT * FROM Timesheet WHERE id = $timesheetId';
+  const values = {$timesheetId: timesheetId};
+  db.get(sql, values, (error, timesheet) => {
+    if(error) {
+      next(error);
+    } else {
+      if (timesheet) {
+        req.timesheet = timesheet;
+        next();
+      } else {
+        res.sendStatus(404);
+      }
+    }
+  });
+});
+
 timesheetsRouter.get('/', (req, res, next) => {
   db.all('SELECT * FROM Timesheet WHERE employee_id = $employeeId', {$employeeId: req.params.employeeId}, (error, timesheets) => {
     if (error) {
@@ -41,6 +58,40 @@ timesheetsRouter.post('/', (req, res, next) => {
           next(error);
         } else {
           res.status(201).json({timesheet: timesheet});
+        }
+      });
+    }
+  });
+});
+
+timesheetsRouter.put('/:timesheetId', (req, res, next) => {
+  const hours = req.body.timesheet.hours;
+  const rate = req.body.timesheet.rate;
+  const date = req.body.timesheet.date;
+  const employeeId = req.params.employeeId;
+
+  if (!hours || !rate || !date) {
+    res.sendStatus(400);
+  }
+
+  const sql = 'UPDATE Timesheet SET hours = $hours, rate = $rate, date = $date, employee_id = $employeeId WHERE Timesheet.id = $timesheetId';
+  const values = {
+    $hours: hours, 
+    $rate: rate, 
+    $date: date, 
+    $employeeId: employeeId, 
+    $timesheetId: req.params.timesheetId
+  };
+
+  db.run(sql, values, (error, timesheet) => {
+    if (error) {
+      next(error);
+    } else {
+      db.get('SELECT * FROM Timesheet WHERE id = $timesheetId', {$timesheetId: req.params.timesheetId}, (error, timesheet) => {
+        if (error) {
+          next(error);
+        } else {
+          res.status(200).json({timesheet: timesheet});
         }
       });
     }
