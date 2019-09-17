ff7f4504ab3f9cdcb15e628328f7f5ffedd19da8
chrissyalbert <chrissy_albert@yahoo.com>
2019-09-17 15:47:19 -0700

    create menusRouter and add get route for all menus

---
+++ api/menus.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const menusRouter = express.Router();
+
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+menusRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Menu', (error, menus) => {
+    if (error) {
+      next(error);
+    } else {
+      res.status(200).json({menus: menus});
+    }
+  });
+});
+
+
+
+module.exports = menusRouter;
