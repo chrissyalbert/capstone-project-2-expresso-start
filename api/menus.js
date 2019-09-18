40a53522168a7aee32aa764380ef15949e8d6378
chrissyalbert <chrissy_albert@yahoo.com>
2019-09-18 13:58:55 -0700

    add menusRouter param '/:menuId' and get route

--- api/menus.js
+++ api/menus.js
@@ -4,6 +4,21 @@ const menusRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menusRouter.param('menuId', (req, res, next, menuId) => {
+  db.get('SELECT * FROM Menu WHERE id = $menuId', {$menuId: menuId}, (error, menu) => {
+    if(error) {
+      next(error);
+    } else {
+      if (menu) {
+        req.menu = menu;
+        next();
+      } else {
+        res.sendStatus(404);
+      }
+    }  
+    });
+});
+
 menusRouter.get('/', (req, res, next) => {
   db.all('SELECT * FROM Menu', (error, menus) => {
     if (error) {
@@ -37,6 +52,10 @@ menusRouter.post('/', (req, res, next) => {
       });
     }
   });
+});
+
+menusRouter.get('/:menuId', (req, res, next) => {
+  res.status(200).json({menu: req.menu});
 });
 
 module.exports = menusRouter;
