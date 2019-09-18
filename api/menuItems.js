afde32943d0069ea5cfb6f79670827ef21db6e4c
chrissyalbert <chrissy_albert@yahoo.com>
2019-09-18 15:38:06 -0700

    Add menuItemsRouter post route

--- api/menuItems.js
+++ api/menuItems.js
@@ -14,4 +14,39 @@ menuItemsRouter.get('/', (req, res, next) => {
   });
 });
 
+menuItemsRouter.post('/', (req, res, next) => {
+  const name = req.body.menuItem.name;
+  const inventory = req.body.menuItem.inventory;
+  const price = req.body.menuItem.price;
+  const description = req.body.menuItem.description;
+  const menuId = req.params.menuId;
+
+  if (!name || !inventory || !price) {
+    res.sendStatus(400);
+  }
+
+  const sql = 'INSERT INTO MenuItem (name, inventory, price, description, menu_id) VALUES ($name, $inventory, $price, $description, $menuId)';
+  const values = {
+    $name: name, 
+    $inventory: inventory, 
+    $price: price, 
+    $description: description, 
+    $menuId: menuId
+  };
+
+  db.run(sql, values, function(error) {
+    if(error) {
+      next(error);
+    } else {
+      db.get(`SELECT * FROM MenuItem WHERE id = ${this.lastID}`, (error, item) => {
+        if (error) {
+          next(error);
+        } else {
+          res.status(201).json({menuItem: item});
+        }
+      });
+    }
+  });
+});
+
 module.exports = menuItemsRouter;
