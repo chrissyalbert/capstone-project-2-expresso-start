52a8181ed8b7c219458b113291d8ebc387282251
chrissyalbert <chrissy_albert@yahoo.com>
2019-09-18 16:26:57 -0700

    Add menuItemsRouter param and put route for '/:menuItemId'

--- api/menuItems.js
+++ api/menuItems.js
@@ -4,6 +4,18 @@ const menuItemsRouter = express.Router({mergeParams: true});
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+menuItemsRouter.param('menuItemId', (req, res, next, menuItemId) => {
+  db.get('SELECT * FROM MenuItem WHERE id = $menuItemId', {$menuItemId: menuItemId}, (error, menuItem) => {
+    if(error) {
+      next(error);
+    } else if (menuItem) {
+      next();
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
 menuItemsRouter.get('/', (req, res, next) => {
   db.all('SELECT * FROM MenuItem WHERE MenuItem.menu_id = $menuId', {$menuId: req.params.menuId}, (error, items) => {
     if (error) {
@@ -47,6 +59,43 @@ menuItemsRouter.post('/', (req, res, next) => {
       });
     }
   });
+});
+
+menuItemsRouter.put('/:menuItemId', (req, res, next) => {
+  const name = req.body.menuItem.name;
+  const inventory = req.body.menuItem.inventory;
+  const price = req.body.menuItem.price;
+  const description = req.body.menuItem.description;
+  const menuId = req.params.menuId;
+  const menuItemId = req.params.menuItemId;
+
+  if (!name || !inventory || !price) {
+    res.sendStatus(400);
+  }
+
+  const sql = 'UPDATE MenuItem SET name = $name, inventory = $inventory, price = $price, description = $description, menu_id = $menuId WHERE id = $menuItemId';
+  const values = {
+    $name: name, 
+    $inventory: inventory,
+    $price: price, 
+    $description: description, 
+    $menuId: menuId,
+    $menuItemId: menuItemId
+  };
+
+  db.run(sql, values, error => {
+    if (error) {
+      next(error);
+    } else {
+      db.get('SELECT * FROM MenuItem WHERE id = $menuItemId', {$menuItemId: menuItemId}, (error, item) => {
+        if (error) {
+          next(error);
+        } else {
+          res.status(200).json({menuItem: item});
+        }
+      });
+    }
+  })
 });
 
 module.exports = menuItemsRouter;
